9abd85bdfb297ee1aa2acbba28d1a704d4856502
Alex <alexjimenez@ig.com.br>
2012-12-02 00:45:13 -0200

    Atualizacao form.js

--- src/main/webapp/jogo/js/forms.js
+++ src/main/webapp/jogo/js/forms.js
@@ -1,4 +1,7 @@
-
+/*eu não jogo mais para o .jsp direto
+sempre passa pelo .action
+seguinte
+nos locais... faz um link para "/jogo/traces.action?locationId=X"*/
  $(document).ready(function(){
 		
 		$('#frmLogin').validate({
@@ -55,5 +58,85 @@
             }
  			
         });
+		
+		$('a[class^="buscaPista"]').click(function(){
+			var b = $(this).attr("class");
+			var l = b.split("_");
+			var ln = l[1];
+			var lnk = "/jogo/foundTrace.action?traceId="+b;
+			var nome = $(this).attr("id");
+			
+			$.ajax({
+				type: "POST",
+                url: "/jogo/foundTrace.action?traceId="+ln,
+                
+				success: function(r){ 
+					if(r=="P"){
+						alert("Pista Encontrada!: "+nome);
+						$(this).css("display","none");
+						location.reload();
+					}
+					if(r=="N"){
+						
+						alert("Nenhuma pista encontrada");
+					}
+				    
+				    
+				},
+				error: function(){  
+				  
+				}
+			});
+		});
+		
+		$('input[name="btCafe"]').click(function(){
+			
+			$.ajax({
+				type: "POST",
+                url: "/jogo/drinkCoffee.action",
+                
+				success: function(r){ 
+					if(r=="E"){
+						alert("Você bebeu café!");
+						
+						location.reload();
+					}
+					if(r=="W"){
+						
+						alert("Acabou o café");
+					}
+				    
+				    
+				},
+				error: function(){  
+				  
+				}
+			});
+			
+		});
+		
+		$('input[name="btProcessar"]').click(function(){
+			
+			$.ajax({
+				type: "POST",
+                url: "/jogo/processTraces.action",
+                
+				success: function(r){ 
+					if(r=="R"){
+						alert("Caso Resolvido!");
+					}
+					if(r=="F"){
+						
+						alert("Pista Processada");
+					}
+					location.reload();
+				    
+				},
+				error: function(){  
+				  
+				}
+			});
+			
+		});
 
     });
